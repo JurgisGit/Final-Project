9bbb4cc61dcb83ef0bcffd92db0a484713106ba4
JurgisGit <lt.jurgis@gmail.com>
2022-10-22 15:36:56 +0300

    filtering authenticated user

--- notes/models.py
+++ notes/models.py
@@ -1,4 +1,12 @@
+from django.conf import settings
 from django.db import models
 from django.core.validators import *
 
-# Create your models here.
+class Note(models.Model):
+    title = models.CharField(max_length=200)
+    text = models.CharField(max_length=900)
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        null=True,
+    )
--- notes/views.py
+++ notes/views.py
@@ -2,16 +2,19 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.shortcuts import redirect
 from django.template import loader
-# from .models import
+from .models import Note
 from django.contrib.auth.forms import User
 from django.contrib.auth import logout, authenticate, login
 from django.contrib.auth.decorators import login_required
 
 
 def index(request):
-    template = loader.get_template('index.html')
-    context = {}
-    return HttpResponse(template.render(context, request))
+    if request.user.is_authenticated:
+        template = loader.get_template('index.html')
+        context = {'notes': Note.objects.filter(user=request.user)}
+        return HttpResponse(template.render(context, request))
+    else:
+        return redirect('login')
 
 
 def login_user(request):
